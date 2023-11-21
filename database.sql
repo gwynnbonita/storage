d927ade7b710bfcb00b5497ed647540302a52c62
Hera Bonita <116147053+gwynnbonita@users.noreply.github.com>
2023-11-21 13:08:22 +0800

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE USERACCOUNT (
+  USER_ID CHAR(4) PRIMARY KEY,
+  USER_FNAME VARCHAR(25) NOT NULL,
+  USER_LNAME VARCHAR(50) NOT NULL,
+  USER_EMAIL VARCHAR(50) NOT NULL,
+  USER_PWORD VARCHAR(15) NOT NULL,
+  USER_PNUMBER CHAR(10) NOT NULL
+);
+
+INSERT INTO USERACCOUNT VALUES (0001, 'Sharia', 'Albano', 'shariaalbano@cc4154.infoman.ph', 'eXADOgeSmudO', '9271008328');
+INSERT INTO USERACCOUNT VALUES (0002, 'Hera Gwynn', 'Bonita', 'gwynnbonita@cc4154.infoman.ph', '=O9644EBo8=', '9175405649');
+INSERT INTO USERACCOUNT VALUES (0003, 'Louie Franz', 'Bustamante', 'louiebustamante@cc4154.infoman.ph', 'RUeNTRYMaLie', '9608864684');
+INSERT INTO USERACCOUNT VALUES (0004, 'Ivy Kyle', 'Delos Santos', 'ivyds@cc4154.infoman.ph', 'UnKLEteRSElA', '9360354516');
+INSERT INTO USERACCOUNT VALUES (0005, 'Denesse Anne', 'Ferrer', 'densferrer@cc4154.infoman.ph', 'uRMiSTeNtHEy', '9595763202');
+INSERT INTO USERACCOUNT VALUES (0006, 'Charmaine', 'Macapagong', 'charmaine@cc4151.infoman.ph', 'exUaINGuSchI', '9654882349');
+INSERT INTO USERACCOUNT VALUES (0007, 'Yracq', 'Mirafuente', 'yracqmirafuente@cc4151.infoman.ph', 'HAdROineyrEF', '9100562579');
+INSERT INTO USERACCOUNT VALUES (0008, 'Brandon Rayne', 'Yarcia', 'brandonyarcia@cc4151.infoman.phh', 'SwarksOTIcKf', '9781223153');
+
